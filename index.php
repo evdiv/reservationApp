72b1a517e6c24c99d1556f5f8de59de07d6812e5
Eugene Dvoir <edvoir@mail.com>
2015-04-06 18:21:40 -0400

    Add reserved days to calendar

--- index.php
+++ index.php
@@ -1,55 +1,44 @@
-<?php 
-//recive data from JSON with reserved days
-//draw calendar and put there reserved days
-//user can choose what dates are needed and send reservation request
-//admin see reservation request and approve it or decline it
-
-
-?>
-
 <!DOCTYPE html>
 <html>
 <head>
-   <title>Condo Reservation App</title>
-   <meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0">
-   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-   <script src="https://cdnjs.cloudflare.com/ajax/libs/knockout/3.3.0/knockout-min.js"></script>
+    <title>Condo Reservation App</title>
+    <meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/knockout/3.3.0/knockout-min.js"></script>
 
-   <script src="js/scripts.js"></script>
+    <script src="js/scripts.js"></script>
   
-   <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
-   <link rel="stylesheet" href="css/style.css" />
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/style.css" />
 </head>
 <body>
 <div id="wrap">
-   <header>
-      <h1>Condo Reservation Calendar</h1>
+    <header>
+        <h1>Condo Reservation Calendar</h1>
 
-      <span class='top-menu' data-bind='click: changeCondo' data-condo='1'>Condo 1</span>
-      <span class='top-menu' data-bind='click: changeCondo' data-condo='2'>Condo 2</span>
-      <span class='top-menu' data-bind='click: changeCondo' data-condo='3'>Condo 3</span>
+        <span class='top-menu' data-bind='click: changeCondo' data-condo='1'>Condo 1</span>
+        <span class='top-menu' data-bind='click: changeCondo' data-condo='2'>Condo 2</span>
+        <span class='top-menu' data-bind='click: changeCondo' data-condo='3'>Condo 3</span>
+    </header>
 
-   </header>
-   <div id="calendar"></div>
+    <div id="calendar"></div>
 
-   <div id="sidebar">
-   <div id="price"></div>
-	
+    <div id="sidebar">
+    <div id="price"></div>
+    
+    <div id="form">
 
-
-	<div id="form">
-
-	<div id="reservedDays" data-bind="foreach: reservedDays">
-      <span class='reservedDate' data-bind='click: $root.removeDate'>
-      	<i class="fa fa-times fa-lg red removeDate"></i>
-      	<span data-bind='text: date'></span>
+    <div id="reserved-days" data-bind="foreach: reservedDays">
+      <span class='reserved-date' data-bind='click: $root.removeDate'>
+        <i class="fa fa-times fa-lg red removeDate"></i>
+        <span data-bind='text: date'></span>
         <span data-bind='text: name'></span>
        </span>
    </div>
 
-	<div id="total" data-bind='visible: reservedDays().length > 0'>Total: $
-	 <span data-bind='text: subtotal'></span>
-   	</div>
+    <div id="total" data-bind='visible: reservedDays().length > 0'>Total: $
+     <span data-bind='text: subtotal'></span>
+    </div>
 
       <form name="reservationForm" acton="" method="post" />
 
@@ -59,11 +48,11 @@
          <input type='text' name='userPhone' data-bind='value: userPhone' placeholder='Your Phone Number' />
          
          
-         <div id="addGuest">
-         	<span class='addGuest' data-bind='click: addGuest'><i class="fa fa-plus-circle fa-lg green"></i> Add a guest</span>
+         <div id="add-guest">
+            <span class='add-guest' data-bind='click: addGuest'><i class="fa fa-plus-circle fa-lg green"></i> Add a guest</span>
          </div>
 
-         <table class='guestsList' data-bind="visible: guests().length > 0">
+         <table class='guests-list' data-bind="visible: guests().length > 0">
             <tr>
                <th>First Name</th>
                <th>Last Name</th>
@@ -80,7 +69,7 @@
 
          <button class='btn' data-bind='click: save, enable: guests().length > 0'>Add Reservation</button>
       </form>
-	</div>
+    </div>
    </div>
 </div>
 
@@ -89,7 +78,7 @@
 var GuestsModel = function(condoId) {
     var self = this;
 
-   	self.condoId = condoId || "1";
+    self.condoId = condoId || '1';
 
     self.userFirstName = ko.observable("");
     self.userLastName = ko.observable("");
@@ -104,21 +93,21 @@ var GuestsModel = function(condoId) {
     self.guests = ko.observableArray();
     self.reservedDays = ko.observableArray();
 
-	self.reservedWeekDays = ko.observable(0);
+    self.reservedWeekDays = ko.observable(0);
     self.reservedWeekEndDays = ko.observable(0);
 
     self.subtotal = ko.pureComputed(function() {
-    	var total = 0;
-    	if(self.reservedDays().length > 0) {
-    		//calculate total tost for weekdays
-    		if(self.reservedWeekDays() > 0) {
-    			total += self.reservedWeekDays() * self.weekDay();
-    		}
-    		//calculate total cost for weekend days 
-    		if(self.reservedWeekEndDays() > 0) {
-    			total += self.reservedWeekEndDays() * self.weekEndDay();
-    		}
-    	}
+        var total = 0;
+        if(self.reservedDays().length > 0) {
+            //calculate total tost for weekdays
+            if(self.reservedWeekDays() > 0) {
+                total += self.reservedWeekDays() * self.weekDay();
+            }
+            //calculate total cost for weekend days 
+            if(self.reservedWeekEndDays() > 0) {
+                total += self.reservedWeekEndDays() * self.weekEndDay();
+            }
+        }
         return total;
     });
 
@@ -139,11 +128,13 @@ var GuestsModel = function(condoId) {
         //Remove active from Calendar
         $(".day[data-date='" + date.date + "'").removeClass('active');
 
+
         if(self.isWeekEnd(date.date)) {
-        	self.reservedWeekEndDays(self.reservedWeekEndDays() - 1);
+            self.reservedWeekEndDays(self.reservedWeekEndDays() - 1);
         } else {
-        	self.reservedWeekDays(self.reservedWeekDays() - 1);
+            self.reservedWeekDays(self.reservedWeekDays() - 1);
         }
+
    };
 
    self.removeDay = function(date) {
@@ -172,83 +163,80 @@ var GuestsModel = function(condoId) {
       }
 
       if(!valueExist) {
-      		if(self.isWeekEnd(date)) {
-    			self.reservedWeekEndDays(self.reservedWeekEndDays() + 1);
-      		} else {
-      			self.reservedWeekDays(self.reservedWeekDays() + 1);
-      		}
+            if(self.isWeekEnd(date)) {
+                self.reservedWeekEndDays(self.reservedWeekEndDays() + 1);
+            } else {
+                self.reservedWeekDays(self.reservedWeekDays() + 1);
+            }
 
          self.reservedDays.push({date: date, name: self.getDayOfWeek(date)});
       } else { 
-         	if(self.isWeekEnd(date)) {
-    			self.reservedWeekEndDays(self.reservedWeekEndDays() - 1);
-      		} else {
-      			self.reservedWeekDays(self.reservedWeekDays() - 1);
-      		}       
+            if(self.isWeekEnd(date)) {
+                self.reservedWeekEndDays(self.reservedWeekEndDays() - 1);
+            } else {
+                self.reservedWeekDays(self.reservedWeekDays() - 1);
+            }       
          self.removeDay(date);
       }
    };
 
    self.getDayOfWeek = function(date) {
-   	  var tmpDate;
-   	  var tmpMonth;
-   	  var weekDayNames = ['San', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+      var tmpDate;
+      var tmpMonth;
+      var weekDayNames = ['San', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
-   	  tmpDate = date.split("-");
-   	  tmpMonth = tmpDate[1] - 1;
-   	  var tmpDay = new Date(tmpDate[0], tmpMonth, tmpDate[2]);
-	  
-	  return weekDayNames[tmpDay.getDay()];
+      tmpDate = date.split("-");
+      tmpMonth = tmpDate[1] - 1;
+      var tmpDay = new Date(tmpDate[0], tmpMonth, tmpDate[2]);
+      
+      return weekDayNames[tmpDay.getDay()];
    };
 
    self.isWeekEnd = function(date) {
-   	  var tmpDate;
-	  var tmpMonth;
+      var tmpDate;
+      var tmpMonth;
 
-   	  tmpDate = date.split("-");
-   	  tmpMonth = tmpDate[1] - 1;
+      tmpDate = date.split("-");
+      tmpMonth = tmpDate[1] - 1;
 
-   	  var weekDay = new Date(tmpDate[0], tmpMonth, tmpDate[2]);
-   	  if(weekDay.getDay() === 0 || weekDay.getDay() === 6) {
-   	  	return true;
-   	  }
-   	  return false;
+      var weekDay = new Date(tmpDate[0], tmpMonth, tmpDate[2]);
+      if(weekDay.getDay() === 0 || weekDay.getDay() === 6) {
+        return true;
+      }
+      return false;
    };
 
    self.changeCondo = function(data, event) {
 
-   	var condoId = $(event.target).attr('data-condo');
+    var condoId = $(event.target).attr('data-condo');
 
-   	//Remove all data related to reserved Days
+    //Remove all data related to reserved Days
     self.reservedDays.removeAll();
-	self.reservedWeekDays(0);
+    self.reservedWeekDays(0);
     self.reservedWeekEndDays(0);
 
-    //Remove all active days from calendar
-    $(".day").removeClass('active');
-
-   	self.getCondoRates(condoId);
+    self.getCondoRates(condoId);
    };
 
    self.getCondoRates = function(condoId) {
 
-   		switch(condoId) {
-   			case "1":
-   			    self.weekDay(50);
-    			self.weekEndDay(70);
-    			self.hotSeason(95);
-    			break;
-   			case "2":
-   			    self.weekDay(56);
-    			self.weekEndDay(78);
-    			self.hotSeason(115);
-    			break;
-    	   	case "3":
-   			    self.weekDay(68);
-    			self.weekEndDay(94);
-    			self.hotSeason(135);
-    			break;		
-   		}
+        switch(condoId) {
+            case "1":
+                self.weekDay(50);
+                self.weekEndDay(70);
+                self.hotSeason(95);
+                break;
+            case "2":
+                self.weekDay(56);
+                self.weekEndDay(78);
+                self.hotSeason(115);
+                break;
+            case "3":
+                self.weekDay(68);
+                self.weekEndDay(94);
+                self.hotSeason(135);
+                break;      
+        }
    };
 
    self.save = function() {
--- js/scripts.js
+++ js/scripts.js
@@ -1,238 +1,208 @@
-$(document).ready(function(){
+$(document).ready(function() {
+    var getData = function(condoId) {
+        var id = condoId || '1';
 
-  var getData = function (condoId){
-      var id = condoId || 1;
-      var condoObj;
-
-      $.getJSON("http://localhost/ReservationCalendar/api/", function(json) {
-
-        for(var i = 0, len = json.length; i < len; i++) {
-          if(json[i].id == id) {
-            condoObj = json[i];
-            break;
-          }
-        }
-
-        printCalendar(condoObj);
-        printPrice();
-        addUserHandlers();
-
-        //Initial App loading
-        $('.top-menu[data-condo |= "1"]').addClass('menu-active');
-        ko.applyBindings(new GuestsModel("1"));
-
-      });
-  };
-
-  var printCalendar = function (condoObj) {
-       var today = getCurrentDate();
-       var reservedDays = condoObj.booked || "";
-       var reservedDaysArray;
-       var condoTitle = condoObj.title || "";
-       var condoDescr = condoObj.description || "";
-       var numWeeks = 0;
-       var numCells = 0;
-       var dayOfMonth = 0;
-       var firstOfMonth;
-       var lastOfMonth;
-       var currentDay;
-       var calendar = $('#calendar');
-       var year = $('<div>').addClass('year');
-       var month;
-       var monthTitle;
-       var daysTitle;
-       var weekDayTitle;
-       var week;
-       var day;
-  
-       //convert Array of strings (reservedDays) to Array of dates to make it easier to search later
-       for(var i = 0, len = reservedDays.length; i < len; i++ ) {
-          if(!reservedDaysArray || reservedDaysArray.length === 0) {
-            reservedDaysArray = reservedDays[i].split(",");
-          } else {
-            reservedDaysArray = reservedDaysArray.concat(reservedDays[i].split(","));
-          }
-        }  
-
-       for(i = 1; i < 13; i++) {
-
-          //Remove all past monthes
-          if(i < today.mm) {
-            continue;
-          }
-
-          month = $('<div>').addClass('month').attr('data-month', i);
-
-          dayOfMonth = 0;
-
-          numWeeks = getNumberOfWeeks(today.yyyy, i);
-          numCells = 1;  
-
-          firstOfMonth = new Date(today.yyyy, i-1, 1);
-          lastOfMonth = new Date(today.yyyy, i, 0); //0 current is a last of previous
-
-          monthTitle = $("<div class='monthName'>" + getMonthName(i-1) + "</div>");
-
-          daysTitle = $("<div class='daysTitle'></div>");
-          for(var d =0; d < 7; d++) {
-            weekDayTitle = "<div class='weekDayTitle'>" + getWeekDayName(d) + "</div>";
-            daysTitle.append(weekDayTitle);
-          }
-
-          month.append(monthTitle);
-          month.append(daysTitle);
-
-           for(var j = 1; j < numWeeks + 1; j++) {
-            week = $('<div>').addClass('week');
-
-            for(d = 1; d < 8; d++) {
-              day = $('<div>').addClass('cell');
-
-              if(numCells == (firstOfMonth.getDay() + 1)) { //find the first day of month
-                dayOfMonth = 1;
-                day.text(dayOfMonth);
-                day.addClass('day');
-
-                day.attr('data-date', today.yyyy + "-" + i + "-" + dayOfMonth);
-    
-                currentDay = new Date(today.yyyy, i-1, dayOfMonth);
-                if(isWeekEnd(currentDay)) {
-                  day.addClass('weekend');
-                }
-
-                //knockout JS
-                day.attr('data-bind', 'click: addDay'); 
-
-                dayOfMonth++;
-
-              } else if(dayOfMonth && dayOfMonth <= lastOfMonth.getDate()) {
-                day.text(dayOfMonth);
-                day.addClass('day');
-
-                day.attr('data-date', today.yyyy + "-" + i + "-" + dayOfMonth);
-
-
-                currentDay = new Date(today.yyyy, i-1, dayOfMonth);
-                if(isWeekEnd(currentDay)) {
-                  day.addClass('weekend');
-                }
-
-                //knockout JS
-                day.attr('data-bind', 'click: addDay'); 
-
-                dayOfMonth++;
-
-              } else {
-                day.text("");
-              }
-
-              numCells ++;
-
-              week.append(day);
+        $.getJSON("http://localhost/ReservationCalendar/api/", function(json) {
+            for (var i = 0, len = json.length; i < len; i++) {
+                    printCalendar(json[i]);
             }
-            month.append(week);
-          }
-          year.append(month);
-       }
-      calendar.append(year);
-  };
 
-  var printPrice = function() {
-    var weekDay = 0,
-        weekEndDay =0,
-        hotSeasonWeekDay = 0,
-        hotSeasonWeekEndDay = 0,
-        html = "";
+            showCalendar(id);
 
-    html += "<div id='rateTitle'>Rates</div>";
-    html += "<div id='weekDay'>Weekday Rate: $ <span data-bind='text: weekDay'></span></div>";
-    html += "<div id='weekEndDay'>Weekend Rate: $ <span data-bind='text: weekEndDay'></span></div>";
-    html += "<div id='hotSeason'>Hot Season Rate: $ <span data-bind='text: hotSeason'></span></div>";    
+            printPrice();
+            addUserHandlers();
 
-    $('#price').html(html);
-  };
+            $('.top-menu[data-condo |= ' + id + ']').addClass('menu-active');
 
-  var isWeekEnd = function(currentDay) {
-    if(currentDay.getDay() === 0 || currentDay.getDay() === 6) {
-      return true;
-    }
-    return false;
-  };
-   
-  var getCurrentDate = function() {
-       var today = new Date();
-       var dd = today.getDate();
-       var mm = today.getMonth()+1; 
-       var yyyy = today.getFullYear();
+            if (!condoId) {
+                ko.applyBindings(new GuestsModel());
+            }
+        });
+    };
+    var printCalendar = function(condoObj) {
+        var today = getCurrentDate();
+        var reservedDays = condoObj.booked || "";
+        var reservedDaysArray = [];
+        var condoId = condoObj.id || 1;
+        var condoTitle = condoObj.title || "";
+        var condoDescr = condoObj.description || "";
+        var numWeeks = 0;
+        var numCells = 0;
+        var dayOfMonth = 0;
+        var firstOfMonth;
+        var lastOfMonth;
+        var currentDay;
+        var calendar = $('#calendar');
+        var year = $('<div>').addClass('year');
+        var month;
+        var monthTitle;
+        var daysTitle;
+        var weekDayTitle;
+        var week;
+        var day;
+        //convert Array of strings (reservedDays) to Array of dates to make it easier to search later
+        for (var i = 0, len = reservedDays.length; i < len; i++) {
+            if (!reservedDaysArray || reservedDaysArray.length === 0) {
+                reservedDaysArray = reservedDays[i].split(","); //first line
+            } else {
+                reservedDaysArray = reservedDaysArray.concat(reservedDays[i].split(",")); //second ones
+            }
+        }
+        for (i = 1; i < 13; i++) {
+            //Show months only from current one
+            if (i < today.mm) {
+                continue;
+            }
+            month = $('<div>').addClass('month').attr('data-month', i);
+            dayOfMonth = 0;
+            numWeeks = getNumberOfWeeks(today.yyyy, i);
+            numCells = 1;
+            firstOfMonth = new Date(today.yyyy, i - 1, 1);
+            lastOfMonth = new Date(today.yyyy, i, 0); //0 current is a last of previous
+            monthTitle = $("<div class='month-name'>" + getMonthName(i - 1) + "</div>");
+            daysTitle = $("<div class='days-title'></div>");
+            for (var d = 0; d < 7; d++) {
+                weekDayTitle = "<div class='weekday-title'>" + getWeekDayName(d) + "</div>";
+                daysTitle.append(weekDayTitle);
+            }
+            month.append(monthTitle);
+            month.append(daysTitle);
+            for (var j = 1; j < numWeeks + 1; j++) {
+                week = $('<div>').addClass('week');
+                for (d = 1; d < 8; d++) {
+                    day = $('<div>').addClass('cell');
+                    if (numCells == (firstOfMonth.getDay() + 1)) { //find the first day of month
+                        dayOfMonth = 1;
+                        day.text(dayOfMonth);
+                        day.addClass('day');
+                        day.attr('data-date', today.yyyy + "-" + i + "-" + dayOfMonth);
+                        currentDay = new Date(today.yyyy, i - 1, dayOfMonth);
+                        if (isWeekEnd(currentDay)) {
+                            day.addClass('weekend');
+                        }
+                    } else if (dayOfMonth && dayOfMonth <= lastOfMonth.getDate()) {
+                        day.text(dayOfMonth);
+                        day.addClass('day');
+                        day.attr('data-date', today.yyyy + "-" + i + "-" + dayOfMonth);
+                        currentDay = new Date(today.yyyy, i - 1, dayOfMonth);
+                        if (isWeekEnd(currentDay)) {
+                            day.addClass('weekend');
+                        }
+                    } else {
+                        day.text("");
+                    }
+                    //if the day has been booked already add styles otherwise add Knokcout JS handler
+                    if (dayOfMonth > 0 && dayOfMonth <= lastOfMonth.getDate()) {
+                        if (reservedDaysArray.indexOf(today.yyyy + "-" + (i - 1) + "-" + dayOfMonth) > -1) {
+                            day.addClass('date-booked');
+                        } else {
+                            //knockout JS
+                            day.attr('data-bind', 'click: addDay');
+                            day.addClass('date-available');
+                        }
+                        dayOfMonth++;
+                    }
+                    numCells++;
+                    week.append(day);
+                }
+                month.append(week);
+            }
+            year.append(month);
+        }
+        calendar.append($('<div>').addClass('condo-cal').attr('id', 'condoId_' + condoId).html(year));
+    };
 
-        if(dd<10) {
-            dd='0'+dd;
-        } 
+    var showCalendar = function(id) {
+        //Remove all active days from calendar
+        $(".day").removeClass('active');
 
-        if(mm<10) {
-            mm='0'+mm;
-        } 
+        $('.condo-cal').hide();
+        $('#condoId_' + id).show();
+    };
 
-        today = {dd:dd, mm:mm, yyyy:yyyy};
+
+    var printPrice = function() {
+        var weekDay = 0,
+            weekEndDay = 0,
+            hotSeasonWeekDay = 0,
+            hotSeasonWeekEndDay = 0,
+            html = "";
+        html += "<div id='rate-title'>Rates</div>";
+        html += "<div id='week-day'>Weekday Rate: $ <span data-bind='text: weekDay'></span></div>";
+        html += "<div id='weekend-day'>Weekend Rate: $ <span data-bind='text: weekEndDay'></span></div>";
+        html += "<div id='hot-season'>Hot Season Rate: $ <span data-bind='text: hotSeason'></span></div>";
+        $('#price').html(html);
+    };
+    var isWeekEnd = function(currentDay) {
+        if (currentDay.getDay() === 0 || currentDay.getDay() === 6) {
+            return true;
+        }
+        return false;
+    };
+    var getCurrentDate = function() {
+        var today = new Date();
+        var dd = today.getDate();
+        var mm = today.getMonth() + 1;
+        var yyyy = today.getFullYear();
+        if (dd < 10) {
+            dd = '0' + dd;
+        }
+        if (mm < 10) {
+            mm = '0' + mm;
+        }
+        today = {
+            dd: dd,
+            mm: mm,
+            yyyy: yyyy
+        };
         return today;
-  };
-   
-  var getNumberOfWeeks = function(year, month) {
-      
-        var firstOfMonth = new Date(year, month-1, 1);
-        var lastOfMonth = new Date(year, month, 0); 
-
+    };
+    var getNumberOfWeeks = function(year, month) {
+        var firstOfMonth = new Date(year, month - 1, 1);
+        var lastOfMonth = new Date(year, month, 0);
         var used = firstOfMonth.getDay() + lastOfMonth.getDate();
-        
-        return Math.ceil( used / 7);
-  };  
+        return Math.ceil(used / 7);
+    };
+    var getMonthName = function(monthId) {
+        var month = [];
+        month[0] = "January";
+        month[1] = "February";
+        month[2] = "March";
+        month[3] = "April";
+        month[4] = "May";
+        month[5] = "June";
+        month[6] = "July";
+        month[7] = "August";
+        month[8] = "September";
+        month[9] = "October";
+        month[10] = "November";
+        month[11] = "December";
+        return month[monthId];
+    };
+    var getWeekDayName = function(weekDayId) {
+        var weekDays = [];
+        weekDays[0] = "Sun";
+        weekDays[1] = "Mon";
+        weekDays[2] = "Tue";
+        weekDays[3] = "Wed";
+        weekDays[4] = "Thu";
+        weekDays[5] = "Fri";
+        weekDays[6] = "Sat";
+        return weekDays[weekDayId];
+    };
+    var addUserHandlers = function() {
+        $('.date-available').on('click', function() {
+            var day = $(this);
+            day.toggleClass('active');
+        });
+        $('.top-menu').on('click', function() {
+            $('.top-menu').removeClass('menu-active');
+            $(this).addClass('menu-active');
+            var condoId = $(this).data('condo');
+            showCalendar(condoId);
+        });
+    };
 
-  var getMonthName = function(monthId) {
-    var month = [];
-    month[0] = "January";
-    month[1] = "February";
-    month[2] = "March";
-    month[3] = "April";
-    month[4] = "May";
-    month[5] = "June";
-    month[6] = "July";
-    month[7] = "August";
-    month[8] = "September";
-    month[9] = "October";
-    month[10] = "November";
-    month[11] = "December";
-
-    return month[monthId];
-  };
-
-  var getWeekDayName = function(weekDayId) {
-    var weekDays = [];
-    weekDays[0] = "Sun";
-    weekDays[1] = "Mon";
-    weekDays[2] = "Tue";
-    weekDays[3] = "Wed";
-    weekDays[4] = "Thu";
-    weekDays[5] = "Fri";
-    weekDays[6] = "Sat";
-
-    return weekDays[weekDayId];
-  };
-
-  var addUserHandlers = function() {
-
-    $('.day').on('click', function() {
-      var day = $(this);
-      day.toggleClass('active');
-    });
-
-    $('.top-menu').on('click', function() {
-      $('.top-menu').removeClass('menu-active');
-      $(this).addClass('menu-active');
-    });
-  };
-
-   
-//Launch App
-getData();
-
+    //Initial App loading
+    getData();
 });
